7f182eb7d804eda8cb135ab6863a71d91b477130
Gunther Schneider <pixeloon@gmail.com>
2016-05-25 16:42:17 -0700

    added submit posts form

--- app.js
+++ app.js
@@ -3,13 +3,24 @@ var app = angular.module('reddit-clone', []);
 
 
 app.controller('MainController', function($scope){
+  window.scope = $scope
   $scope.view = {};
-  $scope.view.posts = {};
-  $scope.view.showAddPostForm === false;
+  $scope.view.posts = [];
+  $scope.view.showAddPostForm = false;
 
 
   $scope.submitPost = function(){
-    $scope.view.post
+    $scope.view.posts.push({"title": $scope.view.inputTitle, "author": $scope.view.inputAuthor, "imageUrl": $scope.view.inputImageUrl, "description": $scope.view.inputDescription})
+    console.log("POSTS: ", $scope.view.posts);
+    $scope.view.showAddPostForm = false;
+    $scope.clearForm();
+  }
+
+  $scope.clearForm = function(){
+    $scope.view.inputTitle = "";
+    $scope.view.inputAuthor  = "";
+    $scope.view.inputImageUrl  = "";
+    $scope.view.inputDescription = "";
   }
 
 
