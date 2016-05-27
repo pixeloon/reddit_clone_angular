0088aa4d9dc9f8caddd856fb62ff391ae4e64082
Gunther Schneider <pixeloon@gmail.com>
2016-05-27 16:10:30 -0700

    added comments

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
 var app = angular.module('reddit-clone', []);
 
 
-app.controller('MainController', function($scope) {
+app.controller('MainController', function($scope, $filter) {
     window.scope = $scope
     $scope.view = {};
     $scope.view.posts = [];
     $scope.view.showAddPostForm = false;
     $scope.view.inputImageUrl = "http://lorempixel.com/200/200/";
     $scope.view.sorting = "Votes";
-
+    $scope.view.search = "";
+    $scope.view.showCommentForm = false;
+    $scope.view.posts.comments = [];
+    $scope.view.showComments = false;
+    $scope.view.postCounter = 0;
 
     $scope.submitPost = function() {
         $scope.view.posts.push({
-            "title": $scope.view.inputTitle,
-            "author": $scope.view.inputAuthor,
-            "imageUrl": $scope.view.inputImageUrl,
-            "description": $scope.view.inputDescription,
-            "votes": 0,
-            "timestamp": Date.now($scope.view.posts)
+            postId: $scope.view.postCounter,
+            title: $scope.view.inputTitle,
+            author: $scope.view.inputAuthor,
+            imageUrl: $scope.view.inputImageUrl,
+            description: $scope.view.inputDescription,
+            votes: 0,
+            timestamp: Date.now(),
+            comments: [],
+            showCommentForm: false,
         });
-        console.log()
         $scope.view.showAddPostForm = false;
+        $scope.view.postCounter = $scope.view.postCounter + 1;
         $scope.clearForm();
     }
 
     $scope.clearForm = function() {
         $scope.view.inputTitle = "Title ";
         $scope.view.inputAuthor = "Gunther";
-        // $scope.view.inputImageUrl  = "";
+        $scope.view.inputImageUrl = "http://lorempixel.com/200/200/";
         $scope.view.inputDescription = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
     }
-
-    $scope.view.posts.sort(function(a, b) {
-        if ($scope.view.sorting = "Votes") {
-            return a.votes - b.votes;
-        }
-        console.log("Sorted by Votes: ", $scope.view.posts)
-    })
 
     $scope.orderPosts = function() {
         if ($scope.view.sorting === "Votes") {
@@ -46,7 +46,13 @@ app.controller('MainController', function($scope) {
         } else if ($scope.view.sorting === "Date") {
             return "timestamp"
         }
+    }
 
+    $scope.submitComment = function(post,comment) {
+        $scope.view.posts[post.postId].comments.push(comment);
+        post.showComments = true;
+        post.showCommentForm = false;
+        post.comment = {}
 
     }
 
